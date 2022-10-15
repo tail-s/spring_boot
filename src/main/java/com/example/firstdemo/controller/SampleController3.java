f19d01f77b1eb4ca976e3b20129966941c27f845
Sojin <voise.ksing@gmail.com>
2022-10-16 00:06:36 +0900

    22.10.16

--- src/main/java/com/example/firstdemo/controller/SampleController3.java
+++ src/main/java/com/example/firstdemo/controller/SampleController3.java
@@ -1,18 +1,22 @@
 package com.example.firstdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.firstdemo.dto.UserDTO;
 
 /*
  * 스프링부트 객체 전달 (userDTO 객체 사용)
  */
 
+
 @Controller
 public class SampleController3 {
 	
+	/*
 	@PostMapping("/SampleController3")
-	public String userForm( String userName, String userId, String userPhone ) {	//이렇게도 작성 합니다~
+	public String userForm( String userName, String userId, String userPhone ) {	//이렇게도 작성 합니다~ (생략 가능)
 //			@RequestParam String userName,
 //			@RequestParam String userId,
 //			@RequestParam String userPhone) {
@@ -25,6 +29,22 @@ public class SampleController3 {
 		
 		return "/post/SampleController3View";
 		
+	}
+	*/
+
+	@PostMapping("/SampleController3")
+	public String userForm(
+			UserDTO userDTO,
+			Model model) {
+		
+		// userDTO 객체를 통해서 출력
+		System.out.println( userDTO.getUserName() );
+		System.out.println( userDTO.getUserId() );
+		System.out.println( userDTO.getUserPhone() );
+		
+		model.addAttribute("heading", "Spring Boot");
+		
+		return "/post/SampleController3View";
 	}
 	
 	
--- src/main/java/com/example/firstdemo/dto/UserDTO.java
+++ src/main/java/com/example/firstdemo/dto/UserDTO.java
@@ -1,5 +1,32 @@
 package com.example.firstdemo.dto;
 
 public class UserDTO {
+	
+	private String userName;
+	private String userId;
+	private String userPhone;
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	
+	
+	
+	
 
 }
